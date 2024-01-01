4c0228b05775a75bda41d2bf82ad670f48dc0aa3
Oğuz Ali Arslan <arslanog20@itu.edu.tr>
2024-01-01 22:51:35 +0300

    structural changes on the code and results are added

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # FNLLM, an LLM approach to Detecting Fake News
-Data Mining course Term Project
-
+FNLLM is a project that aims to determine the usability of zero-shot LLM inference for fake news classification with Llama2 and Mistral. This project utilizes LLMs like Llama2 and Mistral to achieve the results and compare them with ML classifiers and BERT as baseline models.
 
 ## Installation
 To run the classifiers on their own, follow these steps: 
@@ -11,19 +10,30 @@ git clone https://github.com/oguzaliarslan/Group_7
 2. Run the following code to install dependencies:
 ```
 pip install -r requirements.txt
-python app.py
 ```
-3. Scripts we have utilized in this projects can be run by their own, below is an example for ML classifiers,
+3. **Datasets** are too big to upload on Github and can be access from the drive link below
+```
+https://drive.google.com/drive/folders/1Yf6UKXsvUZj--joCH8irCIMDhKtK7dYp
+```
+4. **Models** are also too big to upload on GitHub and can be accessed from the drive link below
+```
+https://drive.google.com/drive/folders/1mxFLV7FpvzaIOxebxR2KWDofVS-t1fG-
+```
+5. Results obtained by the trainings and evaluation done by use can be seen in **main.ipynb**
+
+## Executing the scripts
+Training scripts can be runned on their own.
+1. ML classifiers
 ```
 python train_scripts/classifiers.py  --input_data <input_path> --output_folder <output_folder_name> --grid_search
 ```
 The last --grid_search is optional and can be removed if not wanted.
-
-4. Models are too big to upload on GitHub and can be accessed from the drive link below
+2. Llama2 Inference
 ```
-https://drive.google.com/drive/folders/1mxFLV7FpvzaIOxebxR2KWDofVS-t1fG-
+python train_scripts/llama2.py --input_data <input_path>
 ```
-
-
-### Example Results
+3. BERT Train
+```
+python train_scripts/llama2.py --input_data <input_path> --model_name <model_name_from_huggingface>
+```
 
--- train_scripts/llama2.py
+++ train_scripts/llama2.py
@@ -1,4 +1,5 @@
 import torch
+import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import pipeline
 from langchain import HuggingFacePipeline
@@ -6,83 +7,76 @@ from langchain import PromptTemplate,  LLMChain
 import pandas as pd
 from tqdm import tqdm  
 
-# TODO: Create a proper class structure for generalization
-text_dataset = pd.read_csv('dataset_clean.csv', index_col=0)
-text_dataset = text_dataset.sample(frac=1).reset_index(drop=True)
-text_dataset = text_dataset[0:1500]
-df_text = text_dataset['clean_text'].tolist()
-
-token = "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"
-print('tokenizer')
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
-                                          token=token)
-print('model')
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
-                                             device_map='auto',
-                                             torch_dtype=torch.float16,
-                                             token=token,
-                                            #  load_in_8bit=True,
-                                            load_in_4bit=True
-                                             )
-
-
-pipe = pipeline("text-generation",
-                model=model,
-                tokenizer= tokenizer,
-                torch_dtype=torch.bfloat16,
-                device_map="auto",
-                max_new_tokens = 15,
-                num_return_sequences=1,
-                eos_token_id=tokenizer.eos_token_id
-                )
-
-# TODO: Check the prompt below.
-# template = """Task
-# Determine whether the Statement is a
-# lie (Yes) or not (No) based on the Context
-# and other information.
-# # Output format
-# Answer Yes or No as labels
-# # Examples
-# { examples }
-# # Prediction
-# Text: { text }
-# Label:"""
-
-llm = HuggingFacePipeline(pipeline = pipe, model_kwargs = {'temperature':0})
-
-template = """
-Answer the question based on the context. You should follow ALL the following rules when generating and answering.
-- You are an AI assistant for a fake news detection website.
-- Your job is to assess the authenticity of the given input text and classify it as either real or fake news.
-- Only answer with real or fake, as stated previously. Do not respond with anything else.
-- Your answer should be only a single word (real or fake).
-- Always answer in a respectful manner.
-- Please use logic and reasoning while responding to the query.
-
-User Question : {text}
-Your answer:"""
-
-prompt = PromptTemplate(template=template, input_variables=["text"])
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-# even with quantization it is borderline impossible to test all of them with
-text_dataset = text_dataset[0:1500]
-df_text = text_dataset['clean_text'].tolist()
-df_label = text_dataset['label'].tolist()
-pred_df = pd.DataFrame({'text': df_text, 'label': df_label, 'pred_label':None})
-outputs = []
-# Turns out the problem was the lengths of the sentences.. tragic!
-MAX_TEXT_LENGTH = 512  
-
-for i, text in tqdm(enumerate(df_text), total=len(df_text)):
+def generate_predictions(dataset_path):
+    text_dataset = pd.read_csv(dataset_path, index_col=0)
+    text_dataset = text_dataset.sample(frac=1).reset_index(drop=True)
+    text_dataset = text_dataset[0:1500]
     try:
-        if len(text) > MAX_TEXT_LENGTH:
-            text = text[:MAX_TEXT_LENGTH]
-        output = llm_chain.run(text)
-        pred_df.loc[i, 'pred_label'] = output
-        if i % 500 == 0:
-          pred_df.to_csv(f'llama2-{i}.csv')
-    except TypeError:
-        continue
+        df_text = text_dataset['clean_text'].tolist()
+    except:
+        df_text = text_dataset['text'].tolist()
 
-pred_df.to_csv(f'llama2.csv')
+    token = "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"
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token=token)
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
+                                                 device_map='auto',
+                                                 torch_dtype=torch.float16,
+                                                 token=token,
+                                                 #load_in_4bit=True
+                                                 )
+
+    pipe = pipeline("text-generation",
+                    model=model,
+                    tokenizer=tokenizer,
+                    torch_dtype=torch.bfloat16,
+                    device_map="auto",
+                    max_new_tokens=15,
+                    num_return_sequences=1,
+                    eos_token_id=tokenizer.eos_token_id
+                    )
+
+    llm = HuggingFacePipeline(pipeline=pipe, model_kwargs={'temperature': 0})
+
+    template = """
+    Answer the question based on the context. You should follow ALL the following rules when generating and answering.
+    - You are an AI assistant for a fake news detection website.
+    - Your job is to assess the authenticity of the given input text and classify it as either real or fake news.
+    - Only answer with real or fake, as stated previously. Do not respond with anything else.
+    - Your answer should be only a single word (real or fake).
+    - Always answer in a respectful manner.
+    - Please use logic and reasoning while responding to the query.
+    
+    User Question : {text}
+    Your answer:"""
+
+    prompt = PromptTemplate(template=template, input_variables=["text"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    MAX_TEXT_LENGTH = 512
+    pred_df = pd.DataFrame(columns=['text', 'label', 'pred_label'])
+
+    for i, text in tqdm(enumerate(df_text), total=len(df_text)):
+        try:
+            if len(text) > MAX_TEXT_LENGTH:
+                text = text[:MAX_TEXT_LENGTH]
+            output = llm_chain.run(text)
+            pred_df.loc[i] = [text, text_dataset.loc[i, 'label'], output]
+            if i % 500 == 0:
+                pred_df.to_csv(f'llama2-{i}.csv', index=False)
+        except TypeError:
+            continue
+
+    pred_df.to_csv('llama2.csv', index=False)
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate predictions using the specified dataset.")
+    parser.add_argument('--input_data', type=str, help='Path to input data file (CSV)')
+
+    args = parser.parse_args()
+    
+    dataset_path = args.input_data
+
+    generate_predictions(dataset_path)
+
+if __name__ == "__main__":
+    main()
